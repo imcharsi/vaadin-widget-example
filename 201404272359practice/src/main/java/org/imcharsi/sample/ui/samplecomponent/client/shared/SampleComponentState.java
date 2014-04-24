3ca36fb25ed23c7d5fd74beb09e8a93162a4794d
Kang Woo, Lee <imcharsi@hotmail.com>
2014-04-24 12:49:20 +0900

    vaadin widget example

--- 201404272359practice/src/main/java/org/imcharsi/sample/ui/samplecomponent/client/shared/SampleComponentState.java
+++ 201404272359practice/src/main/java/org/imcharsi/sample/ui/samplecomponent/client/shared/SampleComponentState.java
@@ -1,18 +1,14 @@
 package org.imcharsi.sample.ui.samplecomponent.client.shared;
 
 import com.vaadin.shared.AbstractComponentState;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * Created by KangWoo,Lee on 14. 4. 24.
  */
 public class SampleComponentState extends AbstractComponentState {
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
+    @Getter
+    @Setter
     private String text;
 }
